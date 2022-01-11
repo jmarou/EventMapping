fbbe0b7a4a2ad47b3c7102baa07634afaf20e22b
jmarou <johnmaroufidis@gmail.com>
2022-01-11 15:14:55 +0200

    Commit the changes made during Christmas

---
+++ flask-server/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, send_from_directory
+
+# app = Flask(__name__, static_url_path='/', static_folder='../client/build')
+app = Flask(__name__, static_url_path='/', static_folder='../client/public')
+
+# Routes 
+
+@app.route('/')
+def homepage():
+    return send_from_directory("../client/public", "index.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
