d59544bca6eda28eb0aab63b5aa33fc68c5b7cf5
jmarou <johnmaroufidis@gmail.com>
2022-01-11 15:23:49 +0200

    remove of old github inside client folder

---
+++ client/src/EventMapping.js
@@ -0,0 +1,83 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav, Container, Form } from "react-bootstrap";
+import React from "react";
+import axios from "axios";
+import { Switch, Route, Link, useHistory } from "react-router-dom";
+
+
+function EventMapping(props) {
+//   let history = useHistory();
+  let [home, setHome] = React.useState(true);
+  
+  return (
+    <React.Fragment>
+      <div id="mainContainer">
+        <Navbar
+          variant="dark"
+          expand="sm"
+          id="EventMappingNavBar"
+          style={{
+            borderBottom: "2px solid black",
+            backgroundSize: "cover",
+            backgroundColor: "#3681b8",
+          }}
+        >
+          <Container id="navbarContainer">
+            <div
+              style={{
+                display: "block",
+                position: "relative",
+                left: "20px",
+                top: "-5px",
+                zIndex: "1000",
+                height: "50px",
+                marginRight: "50px",
+              }}
+            >
+            </div>
+
+            <Navbar.Toggle
+              aria-controls="basic-navbar-nav"
+              style={{ backgroundColor: "Black", color: "DarkGray" }}
+            />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link
+                  className="my-auto"
+                  onClick={() => {
+                    setHome(true);
+                  }}
+                >
+                  <Link id="homelink" to="/"
+                  style={{textDecoration: "None", color: "Black", fontStyle: "italic"}}>
+                    Home
+                  </Link>                 
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <Container fluid id="internalContainer"> */}
+            <Route path="/" render={() => <LeafletMap />}></Route>
+        </Container>
+      </div>
+
+      <div id="footerContainer">
+        {[
+          0.05, 0.15, 0.15, 0.25, 0.25, 0.35, 0.25, 0.35, 0.25, 0.45, 0.25,
+          0.45, 0.55, 0.45, 0.55,
+        ].map((opac, idx) => (
+          <div
+            className={idx % 2 === 0 ? "footergray" : "footerwhite"}
+            style={{ opacity: opac }}
+          >
+            {" "}
+          </div>
+        ))}
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default EventMapping;
