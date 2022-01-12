215a0e4b4d247ad55a70e7fe0065160b1d13a8fd
jmarou <johnmaroufidis@gmail.com>
2022-01-12 17:28:50 +0200

    finished with the front end

---
+++ flask-server/EventMapping.py
@@ -0,0 +1,17 @@
+import tweepy
+
+# consumer_key = 
+# consumer_secret = 
+# access_token = 
+# access_token_secret = 
+
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_token_secret)
+
+auth = tweepy.Client()
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, send_from_directory
 
 # app = Flask(__name__, static_url_path='/', static_folder='../client/build')
-app = Flask(__name__, static_url_path='/', static_folder='../client/')
+app = Flask(__name__, static_url_path='/', static_folder='../client/build')
 
 # Routes 
 
 @app.route('/')
 def homepage():
-    # return send_from_directory("../client/build", "index.html")
-    return send_from_directory("../client/public", "index.html")
+    return send_from_directory("../client/build", "index.html")
+    # return send_from_directory("../client/public", "index.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
